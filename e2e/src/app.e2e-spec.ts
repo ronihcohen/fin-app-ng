a8c04545dd07086f6af6ea18394888493c322942
ronihacohen <roni@alooma.io>
2018-10-30 04:13:21 +0000

    Update favicon

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to fin-app-ng!');
+    expect(page.getParagraphText()).toEqual('Login with Google');
   });
 });
