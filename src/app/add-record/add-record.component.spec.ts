4cee79d448e30f55a5f71e1857210980c5768b9c
ronihacohen <roni@alooma.io>
2018-10-30 15:33:35 +0000

    Adding tests for AddRecordComponent

--- src/app/add-record/add-record.component.spec.ts
+++ src/app/add-record/add-record.component.spec.ts
@@ -7,6 +7,7 @@ import {
   MatInputModule,
   MatRadioModule,
   MatSelectModule,
+  MatSnackBarModule
 } from '@angular/material';
 
 import { AddRecordComponent } from './add-record.component';
@@ -17,8 +18,13 @@ import { FormBuilder } from '@angular/forms';
 describe('AddRecordComponent', () => {
   let component: AddRecordComponent;
   let fixture: ComponentFixture<AddRecordComponent>;
+  let addRecordSpy;
 
   beforeEach(async(() => {
+
+    const RecordsServiceMock = jasmine.createSpyObj('RecordsService', ['addRecord']);
+    addRecordSpy = RecordsServiceMock.addRecord.and.returnValue(true);
+
     TestBed.configureTestingModule({
       declarations: [AddRecordComponent],
       imports: [
@@ -29,8 +35,9 @@ describe('AddRecordComponent', () => {
         MatInputModule,
         MatRadioModule,
         MatSelectModule,
+        MatSnackBarModule
       ],
-      providers: [{ provide: RecordsService }, FormBuilder, MatSnackBar]
+      providers: [{ provide: RecordsService, useValue: RecordsServiceMock }, FormBuilder, MatSnackBar]
     }).compileComponents();
   }));
 
@@ -42,5 +49,45 @@ describe('AddRecordComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call addRecord on valid submit', () => {
+    const addRecordElement: HTMLElement = fixture.nativeElement;
+    const button = addRecordElement.querySelector('button');
+    const titleInput: HTMLInputElement = addRecordElement.querySelector('#title-input');
+    const amountInput: HTMLInputElement = addRecordElement.querySelector('#amount-input');
+
+    titleInput.value = 'aaa';
+    amountInput.value = '123';
+
+    titleInput.dispatchEvent(new Event('input'));
+    amountInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(button.textContent).toEqual('Submit');
+
+    button.click();
+    expect(addRecordSpy.calls.any()).toBe(true, 'addRecord called');
+    expect(titleInput.value).toBe('');
+  });
+
+  it('should not call addRecord on invalid submit', () => {
+    const addRecordElement: HTMLElement = fixture.nativeElement;
+    const button = addRecordElement.querySelector('button');
+    const titleInput: HTMLInputElement = addRecordElement.querySelector('#title-input');
+    const amountInput: HTMLInputElement = addRecordElement.querySelector('#amount-input');
+
+    titleInput.value = 'aaa';
+    amountInput.value = 'aaa'; // should be a number so the form is invalid
+
+    titleInput.dispatchEvent(new Event('input'));
+    amountInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(button.textContent).toEqual('Submit');
+
+    button.click();
+    expect(addRecordSpy.calls.any()).toBe(false, 'addRecord want called');
+    expect(titleInput.value).toBe('aaa');
   });
 });
