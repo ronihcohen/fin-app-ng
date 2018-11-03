367d5bf6f38eb9c8226fb321028b044e213a69c1
ronihacohen <roni@alooma.io>
2018-11-03 11:38:14 +0000

    Adding records service tests

--- src/app/records.service.spec.ts
+++ src/app/records.service.spec.ts
@@ -2,15 +2,30 @@ import { TestBed } from "@angular/core/testing";
 
 import { RecordsService } from "./records.service";
 import { AngularFirestore } from "@angular/fire/firestore";
+import * as _moment from "moment";
+import { of } from "rxjs";
 
 describe("RecordsService", () => {
-  beforeEach(() =>
+  beforeEach(() => {
+    const AngularFirestoreMock = {
+      collection: () => ({
+        snapshotChanges: () =>
+          of([
+            {
+              payload: { doc: { id: "uid", data: () => ({ title: "test" }) } }
+            }
+          ])
+      })
+    };
     TestBed.configureTestingModule({
-      providers: [{ provide: AngularFirestore }]
-    }));
+      providers: [{ provide: AngularFirestore, useValue: AngularFirestoreMock }]
+    });
+  });
 
   it("should be created", () => {
     const service: RecordsService = TestBed.get(RecordsService);
-    expect(service).toBeTruthy();
+    service
+      .getRecords("family-uid", _moment())
+      .subscribe(result => expect(result).toBeTruthy());
   });
 });
